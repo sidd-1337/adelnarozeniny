6720b1f1ae7085ddd6af44815b3ee192fc1676cc
sidd-1337 <78906039+sidd-1337@users.noreply.github.com>
2025-05-16 00:10:28 +0200

    Updates

--- static/js/952.307c46bf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadelnarozeninyy=self.webpackChunkadelnarozeninyy||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.307c46bf.chunk.js.map
---
+++ static/js/952.aea10b50.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadelnarozeniny=self.webpackChunkadelnarozeniny||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.aea10b50.chunk.js.map
